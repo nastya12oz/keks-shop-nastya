6374fa7fc4e8a130afc15ef09ea5e8755b31420e
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-11-11 16:54:20 -0800

    кнопка оставить и отменить отзыв

--- src/pages/product-screen/product-screen.tsx
+++ src/pages/product-screen/product-screen.tsx
@@ -3,7 +3,7 @@ import ReviewForm from '../../components/review-form/review-form';
 import Footer from '../../components/footer/footer';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../hooks/index';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchProductByIdAction, fetchReviewsAction } from '../../store/api-actions';
 import { getProduct } from '../../store/products-data/products-data.selectors';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
@@ -15,6 +15,12 @@ import Rating from '../../components/rating/rating';
 import { getProductLoadingStatus } from '../../store/products-data/products-data.selectors';
 import Loading from '../../components/loading/loading';
 import AddToFavoritesButtonInProduct from '../../components/add-to-favorites-button-in-product/add-to-favorites-button-in-product';
+import { getAuthorizationStatus } from '../../store/user-process/user-process.selector';
+import { AuthorizationStatus } from '../../const';
+import BackButton from '../../components/back-button/back-button';
+import classNames from 'classnames';
+
+
 // import FilterSort from '../../components/filter-sort/filter-sort';
 // import { sortByDate, sortByRating } from '../../utils/utils';
 // import { getSortingTypeByDate, getSortingTypeByRating } from '../../store/filters-process/filters-process.selectors';
@@ -24,6 +30,8 @@ function ProductScreen(): JSX.Element {
   const {id} = useParams();
   const dispatch = useAppDispatch();
   const isProductLoading = useAppSelector(getProductLoadingStatus);
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
+  const isAuthorized = authorizationStatus === AuthorizationStatus.Auth;
 
   // const navigate = useNavigate();
 
@@ -43,6 +51,13 @@ function ProductScreen(): JSX.Element {
   // const sortedByRating = sortByRating[sortTypeRating](reviews);
   // const sortedByDate = sortByDate[sortTypeDate](sortedByRating);
 
+  const [showReviewForm, setShowReviewForm] = useState(false);
+
+
+  const handleReviewButtonClick = () => {
+    setShowReviewForm(!showReviewForm);
+  };
+
 
   if (!product) {
     return <NotFoundScreen />;
@@ -55,49 +70,52 @@ function ProductScreen(): JSX.Element {
   return(
     <div className="wrapper">
       <Header />
-      <section className="item-details item-details--form-open">
-        <div className="container">
-          <div className="item-details__wrapper">
-            <div className="item-details__top-wrapper">
-              <h2 className="item-details__name">{product.title}</h2><span className="item-details__price">{product.price}</span>
-            </div>
-            <div className="item-details__weight-wrapper"><span className="item-details__weight">{product.weight} грамм</span></div>
-            <div className="item-details__bottom-wrapper">
-              <div className="item-details__image-wrapper">
-                <picture>
-                  <source type="image/webp" srcSet={product.previewImageWebp} />
-                  <img src={product.previewImage} width={241} height={245} alt={product.title} />
-                </picture><span className="item-details__label">Новинка</span>
+      <main>
+        <h1 className="visually-hidden">{isAuthorized ? 'Карточка: пользователь авторизован' : 'Карточка: пользователь не авторизован' }</h1>
+        <BackButton />
+        <section className={classNames('item-details', {'item-details--form-open': isAuthorized})}>
+          <div className="container">
+            <div className="item-details__wrapper">
+              <div className="item-details__top-wrapper">
+                <h2 className="item-details__name">{product.title}</h2><span className="item-details__price">{product.price}</span>
               </div>
-              <div className="item-details__review-wrapper">
-                <div className="star-rating star-rating--big">
-                  <Rating rating={product.rating} />
-
-                  <span className="star-rating__count">{product.reviewCount}</span>
+              <div className="item-details__weight-wrapper"><span className="item-details__weight">{product.weight} грамм</span></div>
+              <div className="item-details__bottom-wrapper">
+                <div className="item-details__image-wrapper">
+                  <picture>
+                    <source type="image/webp" srcSet={product.previewImageWebp} />
+                    <img src={product.previewImage} width={241} height={245} alt={product.title} />
+                  </picture><span className="item-details__label">Новинка</span>
                 </div>
-                <div className="item-details__text-wrapper"><span className="item-details__text">Цитрусовый десерт с тонким сливочным вкусом, лёгкой свежестью и низким содержанием калорий сд</span>
-                  <button className="item-details__more"><span className="visually-hidden">Читать полностью</span>
-                    <svg width="27" height="17" aria-hidden="true">
-                      <use xlinkHref="#icon-more"></use>
-                    </svg>
-                  </button>
-                </div>
-                <div className="item-details__button-wrapper">
-                  <AddToFavoritesButtonInProduct id={id} />
+                <div className="item-details__review-wrapper">
+                  <div className="star-rating star-rating--big">
+                    <Rating rating={product.rating} />
 
-                  <button className="btn btn--second" type="button">Отменить отзыв</button>
+                    <span className="star-rating__count">{product.reviewCount}</span>
+                  </div>
+                  <div className="item-details__text-wrapper"><span className="item-details__text">Цитрусовый десерт с тонким сливочным вкусом, лёгкой свежестью и низким содержанием калорий сд</span>
+                    <button className="item-details__more"><span className="visually-hidden">Читать полностью</span>
+                      <svg width="27" height="17" aria-hidden="true">
+                        <use xlinkHref="#icon-more"></use>
+                      </svg>
+                    </button>
+                  </div>
+                  <div className="item-details__button-wrapper">
+                    <AddToFavoritesButtonInProduct id={id} />
+                    {isAuthorized &&
+                      <button className="btn btn--second" type="button" onClick={handleReviewButtonClick}> {showReviewForm ? 'Отменить отзыв' : 'Оставить отзыв' }</button>}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <ReviewForm />
-      {/* <FilterSort /> */}
-      {hasReviewsError && <ReviewsError id={id}/>}
-      {reviews.length === 0 && !hasReviewsError && <NoReview />}
-      <ReviewsList reviews={reviews} />
-
+        </section>
+        {showReviewForm && <ReviewForm />}
+        {/* <FilterSort /> */}
+        {hasReviewsError && <ReviewsError id={id}/>}
+        {reviews.length === 0 && !hasReviewsError && <NoReview />}
+        <ReviewsList reviews={reviews} />
+      </main>
       <Footer />
     </div>
   );
