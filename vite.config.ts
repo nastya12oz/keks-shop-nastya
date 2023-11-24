1f55d0d4eeea0b3c104c227c832d80b2811e7367
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-11-24 13:50:10 -0800

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const base = process.env.NODE_ENV === 'production' ? '/keks-shop-nastya/' : '/';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
   test: {
     globals: true,
